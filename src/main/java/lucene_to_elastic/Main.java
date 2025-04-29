a00616599c1589015d96bb0ea42bba44fc49ab78
cemilfatih_coskun <cemilfatihcoskun@gmail.com>
2025-04-29 21:21:28 +0300

    ,

--- src/main/java/lucene_to_elastic/Main.java
+++ src/main/java/lucene_to_elastic/Main.java
@@ -111,7 +111,6 @@ public class Main {
     
     public static void main(String[] args) throws Exception {
         Main.handleLogConfig();
-        
-        
+        Main.transferAndGenerateAfterDoneTransfer();
     }
 }
